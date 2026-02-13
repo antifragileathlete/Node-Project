bc56bcd791330e9a0a32bc10068c18e49c1e8118
antifragileathlete <adrianschultze@gmail.com>
2026-02-13 10:35:35 +0100

    add logger logic in main and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from 'nestjs-pino';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [HealthModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [HealthModule, LoggerModule.forRoot({
+        pinoHttp: {
+            level: process.env.LOG_LEVEL || 'info',
+            transport:
+                process.env.NODE_ENV !== 'production'
+                    ? {
+                        target: 'pino-pretty',
+                        options: {
+                            singleLine: true,
+                        },
+                    }
+                    : undefined,
+        },
+    }),],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
